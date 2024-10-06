12d6bcbd9b4d57c31a3b22d046d272318b12d458
nghiatlt3670 <nghialt3670@gmail.com>
2024-10-06 15:01:30 +0700

    Update config setting

--- src/config/predictors.py
+++ src/config/predictors.py
@@ -3,30 +3,34 @@ from core.inference.predictors.gdino_predictor import GDinoPredictor
 from core.inference.predictors.lama_predictor import LaMaPredictor
 from core.inference.predictors.sam2_predictor import SAM2Predictor
 from core.inference.predictors.sd_inpaint_predictor import SDInpaintPredictor
+import hydra
+
+hydra.core.global_hydra.GlobalHydra.instance().clear()
+hydra.initialize_config_module('static/configs')
 
 PREDICTOR_INITS = [
     PredictorInit(
+        type=SAM2Predictor,
+        params={
+            "device": "cuda:3",
+            "config": "sam2_hiera_t.yaml",
+            "checkpoint": "./static/checkpoints/sam2_hiera_tiny.pt",
+        },
+    ),
+    PredictorInit(
         type=GDinoPredictor,
         params={
-            "device": "cuda",
+            "device": "cuda:1",
             "repo_id": "IDEA-Research/grounding-dino-base",
             "box_threshold": 0.35,
             "text_threshold": 0.25,
         },
     ),
-    PredictorInit(type=LaMaPredictor, params={"device": "cuda"}),
-    PredictorInit(
-        type=SAM2Predictor,
-        params={
-            "device": "cuda",
-            "config": "//home/nghialt/projects/chat2edit/ml-service/static/configs/sam2_hiera_t.yaml",
-            "checkpoint": "//home/nghialt/projects/chat2edit/ml-service/static/checkpoints/sam2_hiera_tiny.pt",
-        },
-    ),
+    PredictorInit(type=LaMaPredictor, params={"device": "cuda:1"}),
     PredictorInit(
         type=SDInpaintPredictor,
         params={
-            "device": "cuda",
+            "device": "cuda:3",
             "repo_id": "stabilityai/stable-diffusion-2-inpainting",
         },
     ),
